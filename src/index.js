bfa38114e81c83102d731791aa3691b99e4d0a1d
Antonio Juan Sánchez <antonio.juan@elparking.com>
2019-07-25 14:36:13 +0200

    actualización de dependencias

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import {extname, basename, dirname} from 'path';
+import { extname, basename, dirname } from 'path'
 
-const COMPONENT_ATTRIBUTE = 'dataTest';
-const DATA_ATTRIBUTE = 'data-test';
+const COMPONENT_ATTRIBUTE = 'dataTest'
+const DATA_ATTRIBUTE = 'data-test'
 
 export default function babelPluginReactDataTestCamelcaseComponent({
   types: t,
 }) {
-  function fileDetails({opts: {filename}}) {
+  function fileDetails({ opts: { filename } }) {
     if (filename === 'unknown' || filename == null) {
-      return null;
+      return null
     }
     return {
       directory: basename(dirname(filename)),
       name: basename(filename, extname(filename)),
-    };
+    }
   }
 
   function isExported(path, name) {
@@ -21,10 +21,10 @@ export default function babelPluginReactDataTestCamelcaseComponent({
       path.parentPath.isExportDefaultDeclaration() ||
       path.parentPath.isExportNamedDeclaration()
     ) {
-      return true;
+      return true
     }
 
-    const binding = path.scope.getBinding(name);
+    const binding = path.scope.getBinding(name)
 
     return binding
       ? binding.referencePaths.some((referencePath) =>
@@ -37,89 +37,89 @@ export default function babelPluginReactDataTestCamelcaseComponent({
                 ancestorPath.isExportNamedDeclaration()
             )
         )
-      : false;
+      : false
   }
 
   function evaluatePotentialComponent(path, state) {
-    const name = nameForReactComponent(path, state.file);
-    const overrides = name && getoverrides(name, state.opts.overrides);
+    const name = nameForReactComponent(path, state.file)
+    const overrides = name && getoverrides(name, state.opts.overrides)
 
-    let process;
+    let process
 
     if (overrides != null && overrides.process != null) {
-      process = overrides.process;
+      process = overrides.process
     } else {
       process =
-        name != null && shouldProcessPotentialComponent(path, name, state);
+        name != null && shouldProcessPotentialComponent(path, name, state)
     }
 
     return {
       name: (overrides && overrides.name) || name || '',
       process,
       overrides,
-    };
+    }
   }
 
   function shouldProcessPotentialComponent(path, name, state) {
     if (!path.getFunctionParent().isProgram()) {
-      return false;
+      return false
     }
     if (path.parentPath.isAssignmentExpression()) {
-      return false;
+      return false
     }
 
-    const {onlyRootComponents = false} = state.opts || {};
+    const { onlyRootComponents = false } = state.opts || {}
     if (!onlyRootComponents) {
-      return true;
+      return true
     }
 
-    const details = fileDetails(state.file);
+    const details = fileDetails(state.file)
     if (details == null) {
-      return false;
+      return false
     }
     if (details.name !== 'index' && details.name !== details.directory) {
-      return false;
+      return false
     }
 
-    return isExported(path, name);
+    return isExported(path, name)
   }
 
   function nameForReactComponent(path, file) {
     const {
       parentPath,
-      node: {id},
-    } = path;
+      node: { id },
+    } = path
 
     if (t.isIdentifier(id)) {
-      return id.name;
+      return id.name
     }
 
     if (parentPath.isVariableDeclarator()) {
-      return parentPath.node.id.name;
+      return parentPath.node.id.name
     }
 
-    const details = fileDetails(file);
+    const details = fileDetails(file)
     if (details == null) {
-      return details;
+      return details
     }
 
-    return details.name === 'index' ? details.directory : details.name;
+    return details.name === 'index' ? details.directory : details.name
   }
 
   const returnStatementVisitor = {
     JSXElement(path) {
-      const openingElement = path.get('openingElement');
-      const {node} = openingElement;
+      const openingElement = path.get('openingElement')
+      const { node } = openingElement
 
       // We never want to go into a tree of JSX elements, only ever process the top-level item
-      path.skip();
+      path.skip()
 
       const hasDataAttribute = node.attributes.some((attribute) =>
-        t.isJSXIdentifier(attribute.name, {name: DATA_ATTRIBUTE})
-      );
+        t.isJSXIdentifier(attribute.name, { name: DATA_ATTRIBUTE })
+      )
 
       if (hasDataAttribute) {
-        return;
+        return
       }
 
       node.attributes.forEach((attr) => {
@@ -130,86 +130,86 @@ export default function babelPluginReactDataTestCamelcaseComponent({
               ...attr.name,
               name: DATA_ATTRIBUTE,
             },
-          });
+          })
         }
-      });
+      })
     },
-  };
+  }
 
   const functionVisitor = {
     ReturnStatement(path) {
-      const arg = path.get('argument');
+      const arg = path.get('argument')
 
       if (arg.isIdentifier()) {
-        const binding = path.scope.getBinding(arg.node.name);
+        const binding = path.scope.getBinding(arg.node.name)
         if (binding == null) {
-          return;
+          return
         }
-        binding.path.traverse(returnStatementVisitor);
+        binding.path.traverse(returnStatementVisitor)
       } else {
-        path.traverse(returnStatementVisitor);
+        path.traverse(returnStatementVisitor)
       }
     },
-  };
+  }
 
   const programVisitor = {
     'ClassDeclaration|ClassExpression': (path, state) => {
-      const {process} = evaluatePotentialComponent(path, state);
+      const { process } = evaluatePotentialComponent(path, state)
       if (!process) {
-        return;
+        return
       }
 
       path
         .get('body.body')
         .filter((bodyPath) => {
-          const {key} = bodyPath.node;
+          const { key } = bodyPath.node
           return (
             bodyPath.isClassMethod() && t.isIdentifier(key) && !key.computed
-          );
+          )
         })
         .forEach((renderPath) => {
-          renderPath.traverse(functionVisitor);
-        });
+          renderPath.traverse(functionVisitor)
+        })
     },
     'FunctionDeclaration|FunctionExpression|ArrowFunctionExpression': (
       path,
       state
     ) => {
-      const {name, process, overrides} = evaluatePotentialComponent(
+      const { name, process, overrides } = evaluatePotentialComponent(
         path,
         state
-      );
+      )
       if (!process) {
-        return;
+        return
       }
 
       if (
         path.isArrowFunctionExpression() &&
         !path.get('body').isBlockStatement()
       ) {
-        path.traverse(returnStatementVisitor, {name, source: path, overrides});
+        path.traverse(returnStatementVisitor, { name, source: path, overrides })
       } else {
-        path.traverse(functionVisitor, {name, source: path, overrides});
+        path.traverse(functionVisitor, { name, source: path, overrides })
       }
     },
-  };
+  }
 
   return {
     name: 'babel-plugin-react-component-data-attribute',
     visitor: {
       Program(path, state) {
-        path.traverse(programVisitor, state);
+        path.traverse(programVisitor, state)
       },
     },
-  };
+  }
 }
 
 function getoverrides(component, overrides = {}) {
   const overide = overrides.hasOwnProperty(component)
     ? overrides[component]
-    : {};
+    : {}
   return {
     name: overide.name || component,
     process: overide.process,
-  };
+  }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,84 +1,102 @@
-import * as babel from 'babel-core';
-import {resolve} from 'path';
-import babelPluginReactDataTestCamelcaseComponent from '../src';
+import * as babel from 'babel-core'
+import babelPluginReactDataTestCamelcaseComponent from '../src'
 
 describe('babelPluginReactDataTestCamelcaseComponent()', () => {
   it('handles a data-test explicit definition', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         return <div key='uno' data-test='id-test-2' data-info='my data info'/>;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handles simple returns', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         return <div dataTest="description-id" />;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handles a conlfict', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         return <div data-test='id-test-1' dataTest="description-id" />;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handles simple complex returns', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         const t = 'description-id'
         return <div dataTest={t} />;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handles simple inline complex returns', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         const t = 1
         return <div dataTest={\`description-id-\${t}\`} />;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('does not add attributes to composite components', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         const t = 1
         return <SomeOtherComponent dataTest={\`description-id-\${t}\`}  />;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('adds the attribute to the first non-composite component', () => {
-    expect(transform(`
+    expect(
+      transform(`
       function MyComponent() {
         const t = 1
         return <SomeOtherComponent><div><div dataTest={\`description-id-\${t}\`} /></div></SomeOtherComponent>;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handles a inline component', () => {
-    expect(transform(`
+    expect(
+      transform(`
       const MyComponent2 = () => {
         return <SomeOtherComponent dataTest={\`description-id-\${t}\`}></SomeOtherComponent>;
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handles a inline component without return', () => {
-    expect(transform(`
+    expect(
+      transform(`
       const MyComponent3 = () => <SomeOtherComponent dataTest={\`description-id-\${t}\`}></SomeOtherComponent>;
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handle properly a class render methods', () => {
-    expect(transform(`
+    expect(
+      transform(`
       class MyComponent extends React.Component {
         renderAnotherThing() {
           return <div dataTest="id-info-AnotherThing" />;
@@ -87,11 +105,13 @@ describe('babelPluginReactDataTestCamelcaseComponent()', () => {
           return <div dataTest="id-info">{ this.renderAnotherThing() }</div>;
         }
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handle properly passing attributes beetween class render methods', () => {
-    expect(transform(`
+    expect(
+      transform(`
       class MyComponent1 extends React.Component {
         render() {
           return <div dataTest={this.props.dataTest}>Info</div>;
@@ -111,31 +131,33 @@ describe('babelPluginReactDataTestCamelcaseComponent()', () => {
           return <MyComponent1 dataTest="id-info" />;
         }
       }
-    `)).toMatchSnapshot();
-  });
+    `)
+    ).toMatchSnapshot()
+  })
 
   it('handle when bail early if we are in a different function than the component', () => {
-    expect(transform(`
+    expect(
+      transform(`
       
       class MyComponent2 extends React.Component {
         render() {
           return <MyComponent1 dataTest="id-info" />;
         }
       }
-    `)).toMatchSnapshot();
-  });
-
-});
+    `)
+    ).toMatchSnapshot()
+  })
+})
 
 function transform(code, pluginOptions, transformOptions) {
-  return babel.transform(code, {
-    babelrc: false,
-    plugins: [
-      [babelPluginReactDataTestCamelcaseComponent, pluginOptions],
-    ],
-    parserOpts: {
-      plugins: ['jsx'],
-    },
-    ...transformOptions,
-  }).code.trim();
+  return babel
+    .transform(code, {
+      babelrc: false,
+      plugins: [[babelPluginReactDataTestCamelcaseComponent, pluginOptions]],
+      parserOpts: {
+        plugins: ['jsx'],
+      },
+      ...transformOptions,
+    })
+    .code.trim()
 }
